d37550391322b5d76943de4ffab6949ee591c20f
MariiaFedkiv <olhovetc@gmail.com>
2022-07-21 13:51:39 +0300

    added variable declaration to "curentYear"

--- Lesson21/assets/js/script.js
+++ Lesson21/assets/js/script.js
@@ -5,7 +5,7 @@ let massege = "Hello," + userName;
 console.log(massege);
 
 let userBirthYear = prompt('What is the year of your birth?');
-   curentYear = 2022
+ const curentYear = 2022;
  
 let age = curentYear - userBirthYear;
 
